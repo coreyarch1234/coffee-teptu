087894640a581ee86e5dd58fd65b335a0b3ec896
coreyarch1234 <harrilal@mit.edu>
2016-12-28 19:48:08 -0500

    got rid of skill section requirement and added a title to user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,5 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :show, :edit, :update, :destroy]
-
-  # GET /users
-  # GET /users.json
   def index
     if params[:search]
       @users = User.search(params[:search]).order("created_at DESC")
@@ -11,18 +8,14 @@ class UsersController < ApplicationController
     end
   end
 
-  # GET /users/1
-  # GET /users/1.json
   def show
     @user = User.find(params[:id])
   end
 
-  # GET /users/new
   def new
     @user = User.new
   end
 
-  # GET /users/1/edit
   def edit
     if current_user
        @user = User.find(params[:id])
@@ -31,8 +24,6 @@ class UsersController < ApplicationController
     end
   end
 
-  # POST /users
-  # POST /users.json
   def create
     @user = User.new(user_params)
 
@@ -48,8 +39,6 @@ class UsersController < ApplicationController
     end
   end
 
-  # PATCH/PUT /users/1
-  # PATCH/PUT /users/1.json
   def update
     @user = User.find(params[:id])
     respond_to do |format|
@@ -63,8 +52,6 @@ class UsersController < ApplicationController
     end
   end
 
-  # DELETE /users/1
-  # DELETE /users/1.json
   def destroy
     @user = User.find(params[:id])
     @user.destroy
@@ -75,14 +62,13 @@ class UsersController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_user
       @user = User.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :role, :about_me, :skill, :contact, :admin)
+      params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :role, :about_me, :title, :contact, :admin)
+    #   Took out skill attribute. Remember to delete it completely from model
     end
 
     # Confirms a logged-in user.
